50e550ac88b5924798d4e6b2febe4b94c87fb853
RazvanCiuciu <razvanciuciu2@gmail.com>
2026-07-12 16:02:56 +0300

    chore(solver): annotated OR-Tools VRPTW example with window experiments

---
+++ solver/experiments/vrptw_example.py
@@ -0,0 +1,190 @@
+"""Vehicles Routing Problem (VRP) with Time Windows."""
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data["time_matrix"] = [
+        [0, 6, 9, 8, 7, 3, 6, 2, 3, 2, 6, 6, 4, 4, 5, 9, 7],
+        [6, 0, 8, 3, 2, 6, 8, 4, 8, 8, 13, 7, 5, 8, 12, 10, 14],
+        [9, 8, 0, 11, 10, 6, 3, 9, 5, 8, 4, 15, 14, 13, 9, 18, 9],
+        [8, 3, 11, 0, 1, 7, 10, 6, 10, 10, 14, 6, 7, 9, 14, 6, 16],
+        [7, 2, 10, 1, 0, 6, 9, 4, 8, 9, 13, 4, 6, 8, 12, 8, 14],
+        [3, 6, 6, 7, 6, 0, 2, 3, 2, 2, 7, 9, 7, 7, 6, 12, 8],
+        [6, 8, 3, 10, 9, 2, 0, 6, 2, 5, 4, 12, 10, 10, 6, 15, 5],
+        [2, 4, 9, 6, 4, 3, 6, 0, 4, 4, 8, 5, 4, 3, 7, 8, 10],
+        [3, 8, 5, 10, 8, 2, 2, 4, 0, 3, 4, 9, 8, 7, 3, 13, 6],
+        [2, 8, 8, 10, 9, 2, 5, 4, 3, 0, 4, 6, 5, 4, 3, 9, 5],
+        [6, 13, 4, 14, 13, 7, 4, 8, 4, 4, 0, 10, 9, 8, 4, 13, 4],
+        [6, 7, 15, 6, 4, 9, 12, 5, 9, 6, 10, 0, 1, 3, 7, 3, 10],
+        [4, 5, 14, 7, 6, 7, 10, 4, 8, 5, 9, 1, 0, 2, 6, 4, 8],
+        [4, 8, 13, 9, 8, 7, 10, 3, 7, 4, 8, 3, 2, 0, 4, 5, 6],
+        [5, 12, 9, 14, 12, 6, 6, 7, 3, 3, 4, 7, 6, 4, 0, 9, 2],
+        [9, 10, 18, 6, 8, 12, 15, 8, 13, 9, 13, 3, 4, 5, 9, 0, 9],
+        [7, 14, 9, 16, 14, 8, 5, 10, 6, 5, 4, 10, 8, 6, 2, 9, 0],
+    ]
+    data["time_windows"] = [
+        (0, 5),  # depot
+        (7, 12),  # 1
+        (10, 15),  # 2
+        (5, 9),  # 3
+        (10, 13),  # 4
+        (0, 5),  # 5
+        (5, 10),  # 6
+        (0, 4),  # 7
+        (5, 10),  # 8
+        (0, 3),  # 9
+        (10, 16),  # 10
+        (10, 15),  # 11
+        (0, 5),  # 12
+        (5, 10),  # 13
+        (7, 8),  # 14
+        (10, 15),  # 15
+        (11, 15),  # 16
+    ]
+    data["num_vehicles"] = 4
+    data["depot"] = 0
+    return data
+
+
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()}")
+    time_dimension = routing.GetDimensionOrDie("Time")
+    total_time = 0
+    for vehicle_id in range(data["num_vehicles"]):
+        if not routing.IsVehicleUsed(solution, vehicle_id):
+            continue
+        index = routing.Start(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
+        while not routing.IsEnd(index):
+            time_var = time_dimension.CumulVar(index)
+            plan_output += (
+                f"{manager.IndexToNode(index)}"
+                f" Time({solution.Min(time_var)},{solution.Max(time_var)})"
+                " -> "
+            )
+            index = solution.Value(routing.NextVar(index))
+        time_var = time_dimension.CumulVar(index)
+        plan_output += (
+            f"{manager.IndexToNode(index)}"
+            f" Time({solution.Min(time_var)},{solution.Max(time_var)})\n"
+        )
+        plan_output += f"Time of the route: {solution.Min(time_var)}min\n"
+        print(plan_output)
+        total_time += solution.Min(time_var)
+    print(f"Total time of all routes: {total_time}min")
+
+
+def main():
+    """Solve the VRP with time windows."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    #aici o sa am eu datele mele, time_matrix, time_windows, num_vehicles, depot (start index)
+    #de observat ca in cazul asta am un singur timeWindow per locatie, in timp ce eu posibil sa am mai multe (daca un client nu poate intre 2-5 automat am 2 timewindows)
+ 
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["time_matrix"]), data["num_vehicles"], data["depot"]
+    )
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+    
+    #aici folosesc OR-Tools ca sa creez index manager si routing model 
+    #RoutingIndexManager traduce informatile pe care ii le dau eu intr un mod in care OR-Tools le intelege
+    #RoutingModel creeaza un routin model pe baza managerului 
+
+    # Create and register a transit callback.
+    def time_callback(from_index, to_index):
+        """Returns the travel time between the two nodes."""
+        # Convert from routing variable Index to time matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["time_matrix"][from_node][to_node]
+    #aici mi ar da timpul pe care l as face de la nodul from la nodul to 
+    
+    
+    transit_callback_index = routing.RegisterTransitCallback(time_callback)
+    #OR-Tools e C++ si de aia nu poate vedea functii pyhton direct
+    #RegisterTransitCallback e o predare a functie pe care Or-Tools o inregistrez la el in tabel si im da inapoi un index pentru unde sta functia in tabel
+    
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    # folosind functia time_callback dau o valoare fiecarui drum dintre 2 locatii
+
+
+
+    # Add Time Windows constraint.
+    time = "Time"
+    routing.AddDimension(
+        transit_callback_index, # functia de callback(ce imi da timpul dintr 2 locatii)
+        30,  # cat am voie maxim sa stau dupa un client in cazul meu
+        30,  # cand se termina ziua
+        False,  # nu sunt fortat sa plec cu toate vechiculele la timpul 0
+        time, #numele
+    )
+    #concept central la Or-Tools Routing
+    #o "dimensiune" este o cantitate care se acumuleaza de a lungul rutei, in cazul meu timpul
+
+
+
+    time_dimension = routing.GetDimensionOrDie(time)
+    # Add time window constraints for each location except depot.
+    for location_idx, time_window in enumerate(data["time_windows"]):
+        if location_idx == data["depot"]:
+            continue
+        index = manager.NodeToIndex(location_idx)
+        time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
+    #se pun constrangerile de timp pe fiacere nod
+
+    # Add time window constraints for each vehicle start node.
+    depot_idx = data["depot"]
+    for vehicle_id in range(data["num_vehicles"]):
+        index = routing.Start(vehicle_id)
+        time_dimension.CumulVar(index).SetRange(
+            data["time_windows"][depot_idx][0], data["time_windows"][depot_idx][1]
+        )
+    #aici dau programul de lucru pentru fiecare vehicul
+
+    # Instantiate route start and end times to produce feasible times.
+    for i in range(data["num_vehicles"]):
+        routing.AddVariableMinimizedByFinalizer(
+            time_dimension.CumulVar(routing.Start(i))
+        )
+        routing.AddVariableMinimizedByFinalizer(time_dimension.CumulVar(routing.End(i)))
+    #face ca start-ul si end-ul ficarui vehicul sa fie cea mai mica val posibila
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    #aici setez strategia de rezolvare folosing PATH_CHEAPEST_ARC care este foarte un NN imporved
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+    #aici e rezolvata problema in sine
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == "__main__":
+    main()
+
+
+#   Categorie      intrebarea la care raspunde     COD
+
+#   Constrangeri   ce e permis?                    SetRange, dimensiunea cu plafonul 30
+
+#   Obiectiv       ce e mai bun?                   SetArcCostEvaluator (+ finalizer-ele, rangul 2)
+
+#   Strategie      cum caut?                       PATH_CHEAPEST_ARC
